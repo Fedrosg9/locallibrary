25ec094994cc9692dff2c60891beca9af7713703
synchris <synchris@daemons.gr>
2015-04-19 21:26:39 +0300

    Basic entities book category and rental

---
+++ src/java/entity/Book.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author synchris
+ */
+public class Book {
+    private int id;
+    private int isbn;
+    private String Title;
+    private int edition;
+    private String Author;
+    private int author_id;
+    private String Publisher;
+    private int publisher_id;
+}
