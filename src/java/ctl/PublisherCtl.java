d03dcd2899298b25cbff8dcdb1e2967bfa5dab2d
synchris <synchris@daemons.gr>
2015-04-19 20:43:42 +0300

    Add basic getters and setters for Member and for the other three;

---
+++ src/java/ctl/PublisherCtl.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ctl;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author synchris
+ */
+public class PublisherCtl {
+        public ArrayList<entity.Publisher> List()
+    {
+        return null;
+    }
+    
+    public entity.Publisher Add(entity.Publisher p)
+    {
+        return null;
+    }
+    
+    public boolean Del(entity.Publisher p)
+    {
+        return false;
+    }
+    
+    public entity.Publisher Edit(entity.Publisher p)
+    {
+        return null;
+    }
+}
